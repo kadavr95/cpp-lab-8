739034a494c05fd3bf7c0fa0a365920917b9a285
Yaskovich Dmitry <kadavr95@outlook.com>
2015-11-11 23:37:00 +0300

    initial commit

---
+++ Lab8.cpp
@@ -0,0 +1,29 @@
+#include <iostream.h>
+#include <conio.h>
+
+void main( )
+{
+	  int i=0,j;
+	  char s[100];
+	  char allowed[]="qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890-_.";
+	  cout<<"Enter e-mail: ";
+	  cin.getline(s,100);
+
+	  while (s[i]!='\0')
+		{
+		   j=0;
+		   while (allowed[j]!=0 &&  allowed[j]!=s[i])
+			  j++;
+		   if (allowed[j]=='\0')
+		   {
+			  cout<<"Invalid e-mail address!";
+			  break;
+		   }
+		   i++;
+		}
+	  if (s[i]=='\0')
+	  {
+			cout<<"E-mail is correct";
+	  }
+	  getch();
+}
